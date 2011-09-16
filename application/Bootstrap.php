ffa3ef5c1e710c685ad8d9b1024c8f78e39a77e9
Tim Lytle <tim@timlytle.net>
2011-09-16 00:33:09 -0400

    Twilio

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,11 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+	protected function _initTwilio()
+	{
+		$config = $this->getOption('twilio');
+		$twilio = new Services_Twilio($config['sid'], $config['token']);
+		return $twilio;
+	}
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,27 @@
 <?php
-
 class IndexController extends Zend_Controller_Action
 {
+	protected $twilio;
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->twilio = $this->getFrontController()->getParam('bootstrap')->getResource('twilio');
+        $this->session = new Zend_Session_Namespace('message');
+        $this->getResponse()->setHeader('Content-Type', 'application/xml');
     }
 
-    public function indexAction()
+    public function indexAction() {}
+    
+    public function messageAction()
     {
-        // action body
+        if($this->_hasParam('RecordingUrl')){
+            $this->session->message = $this->_getParam('RecordingUrl');
+            $this->getHelper('redirector')->gotoSimple('thanks');
+        }
     }
-
-
+    
+    public function thanksAction() 
+    {
+        $this->view->message = $this->session->message;
+    }
 }
-
